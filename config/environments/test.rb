80ada9b439252fa5162dd62323824bf27d5da9f6
川島慎悟 <kawashingo1990@gmail.com>
2020-01-30 20:53:03 +0900

    Bullet による N+1 警告の設定

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # The test environment is used exclusively to run your application's
 # test suite. You never need to work with it otherwise. Remember that
 # your test database is "scratch space" for the test suite and is wiped
@@ -45,4 +47,13 @@ Rails.application.configure do
 
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
+
+  # N + 1はポップアップ通知させる
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+  end
 end
