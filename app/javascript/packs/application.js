f22a1d17979491220b7b32d626bb5e92628626b6
川島慎悟 <kawashingo1990@gmail.com>
2020-02-04 23:20:15 +0900

    JSライブラリをwebpackerから動くように変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,21 +8,17 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("materialize-css")
+require("chartist")
+require("chartist-plugin-tooltips")
+require("perfect-scrollbar")
+require("sparkline")
 
 import "../sources/stylesheets/application"
-// all required is
-import "../sources/javascripts/perfect-scrollbar/js/perfect-scrollbar.jquery.min"
-// apps
 import "../sources/javascripts/app"
 import "../sources/javascripts/app.init"
 import "../sources/javascripts/app-style-switcher"
-// custom js
 import "../sources/javascripts/custom.min"
-// this page plugin js
-import "../sources/javascripts/chartist/chartist.min"
-import "../sources/javascripts/chartist-plugin-tooltips/chartist-plugin-tooltip.min"
-import "../sources/javascripts/sparkline/sparkline"
-import "../sources/javascripts/pages/dashboard1"
+import "../sources/javascripts/dashboard1"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
