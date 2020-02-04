f7b9c9893197939d921cd6e09fac8bec037d48c2
川島慎悟 <kawashingo1990@gmail.com>
2020-02-04 10:12:51 +0900

    JSアセットの設定

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,20 @@ require("@rails/activestorage").start()
 require("channels")
 
 import "../sources/stylesheets/application"
+// all required is
+import "../sources/javascripts/materialize.min"
+import "../sources/javascripts/perfect-scrollbar/dist/js/perfect-scrollbar.jquery.min"
+// apps
+import "../sources/javascripts/app"
+import "../sources/javascripts/app.init"
+import "../sources/javascripts/app-style-switcher"
+// custom js
+import "../sources/javascripts/custom.min"
+// this page plugin js
+import "../sources/javascripts/chartist/dist/chartist.min"
+// import "../sources/javascripts/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min"
+import "../sources/javascripts/sparkline/sparkline"
+import "../sources/javascripts/pages/dashboard1"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
