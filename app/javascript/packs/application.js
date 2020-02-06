56cb462a4449fa7cacefcf50ae49881ab1b0eb32
川島慎悟 <kawashingo1990@gmail.com>
2020-02-06 22:26:17 +0900

    うまく読み込めなかったJSライブラリ（chartist, sparkline, perfect-scrollbar）の読み込みを削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,17 +9,12 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("materialize-css")
-require("chartist")
-require("chartist-plugin-tooltips")
-require("perfect-scrollbar")
-require("sparkline")
 
 import "../sources/stylesheets/application"
 import "../sources/javascripts/app"
 import "../sources/javascripts/app.init"
 import "../sources/javascripts/app-style-switcher"
 import "../sources/javascripts/custom.min"
-import "../sources/javascripts/dashboard1"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
