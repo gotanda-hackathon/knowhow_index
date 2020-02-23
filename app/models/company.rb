f80056a072c0dd374a88b2ec45af1563c22f2d2b
川島慎悟 <kawashingo1990@gmail.com>
2020-02-23 14:42:34 +0900

    $ bin/rails db:migrate

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: companies # 利用企業テーブル
+#
+#  id           :bigint           not null, primary key
+#  name(企業名) :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 class Company < ApplicationRecord
 end
