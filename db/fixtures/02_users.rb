a2a556de92e58ed2eeea98f3926c97dad4a9d432
川島慎悟 <kawashingo1990@gmail.com>
2020-02-24 14:35:08 +0900

    ユーザー初期データのセット

---
+++ db/fixtures/02_users.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+User.seed do |s|
+  s.id = 1
+  s.name = Rails.application.credentials.admin[:name]
+  s.email = Rails.application.credentials.admin[:email]
+  s.password = Rails.application.credentials.admin[:password]
+  s.grader = true
+  s.admin = true
+  s.company = Company.find_by(name: 'ワンスター')
+end
