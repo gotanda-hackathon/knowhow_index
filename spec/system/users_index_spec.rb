19a11c0fc6b8aa5e3142a0ef8d2d923ac512b1f8
川島慎悟 <kawashingo1990@gmail.com>
2020-02-29 17:29:05 +0900

    grader 権限の有無で new/edit/destroy のボタン表示を制御し、その users#index の SystemSpec を通す

---
+++ spec/system/users_index_spec.rb
@@ -0,0 +1,78 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'フロント画面：アカウント一覧', type: :system do
+  context 'ログインしているとき' do
+    let!(:user1) { FactoryBot.create(:user, company: login_user.company) }
+    let!(:user2) { FactoryBot.create(:user) }
+
+    before do
+      sign_in_as(login_user)
+      visit company_users_path(login_user)
+    end
+
+    context '権限が not_grader のとき' do
+      let(:login_user) { FactoryBot.create(:user, :not_grader) }
+
+      it 'ログインアカウントと同じ企業アカウントの情報だけが表示されていること' do
+        expect_outline_of(login_user)
+        expect_outline_of(user1)
+        expect_noy_outline_of(user2)
+      end
+
+      it '新規作成ボタンが存在しないこと' do
+        expect(page).not_to have_link '新規作成'
+      end
+
+      it '編集ボタンが存在しないこと' do
+        expect(page).not_to have_link '編集'
+      end
+
+      it '削除ボタンが存在しないこと' do
+        expect(page).not_to have_link '削除'
+      end
+    end
+
+    context '権限が grader のとき' do
+      let(:login_user) { FactoryBot.create(:user, :grader) }
+
+      it 'ログインアカウントと同じ企業アカウントの情報だけが表示されていること' do
+        expect_outline_of(login_user)
+        expect_outline_of(user1)
+        expect_noy_outline_of(user2)
+      end
+
+      it '新規作成ボタンが存在すること' do
+        expect(page).to have_link '新規作成'
+      end
+
+      it '編集ボタンがアカウントの数だけあること' do
+        expect(page).to have_content('編集', count: User.same_as_current_user_company(login_user).count)
+      end
+
+      it '削除ボタンがアカウントの数だけあること' do
+        expect(page).to have_content('削除', count: User.same_as_current_user_company(login_user).count)
+      end
+    end
+  end
+
+  context 'ログインしていないとき' do
+    before do
+      visit company_users_path(FactoryBot.create(:user))
+    end
+
+    it_behaves_like 'ログイン画面にリダイレクト'
+  end
+
+  def expect_outline_of(user)
+    expect(page).to have_content user.name
+    expect(page).to have_content user.email
+    expect(page).to have_content(user.grader? ? 'あり' : 'なし')
+  end
+
+  def expect_noy_outline_of(user)
+    expect(page).not_to have_content user.name
+    expect(page).not_to have_content user.email
+  end
+end
