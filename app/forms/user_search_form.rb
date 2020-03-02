9b0e03f9d8d8c4b4481b74ccd20f8fda81dfaa04
川島慎悟 <kawashingo1990@gmail.com>
2020-03-02 22:37:31 +0900

    users の検索フォームclassをTDDで作成

---
+++ app/forms/user_search_form.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class UserSearchForm
+  include ActiveModel::Model
+
+  attr_accessor :user_ids
+  attr_accessor :email
+  attr_accessor :grader
+
+  def search(current_user)
+    records = User.same_as_current_user_company(current_user)
+    records = scoped_by_user(records)
+    records = scoped_by_email(records)
+    records = scoped_by_grader(records)
+    records.order(:id)
+  end
+
+  private
+
+  def scoped_by_user(records)
+    return records if user_ids.nil? || user_ids.join.empty?
+
+    records.where(id: user_ids)
+  end
+
+  def scoped_by_email(records)
+    return records if email.blank?
+
+    records.where('email LIKE (?)', "%#{email}%")
+  end
+
+  def scoped_by_grader(records)
+    return records if grader.blank?
+
+    records.where(grader: grader)
+  end
+end
