f6af38983858dc6c321f3f899d4501d047221ffa
川島慎悟 <kawashingo1990@gmail.com>
2020-03-12 22:27:54 +0900

    ad_media#index の実装

--- app/controllers/ad_media_controller.rb
+++ app/controllers/ad_media_controller.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
 class AdMediaController < ApplicationController
-  def index; end
+  def index
+    condition = current_user.get_search_condition(code: 'ad_medium', params: search_params.to_unsafe_h)
+    @search_form = AdMediumSearchForm.new(condition)
+    @ad_media = @search_form.search(current_user).page(params[:page]).per(Settings.pagination.default).decorate
+  end
 
   def new; end
 
   def edit; end
+
+  private
+
+  def search_params
+    params.fetch(:search_form, {}).permit(:name)
+  end
 end
