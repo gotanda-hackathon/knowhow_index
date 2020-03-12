66fd0ce6041b9e31828a2a6c82260cd9324823f5
川島慎悟 <kawashingo1990@gmail.com>
2020-03-12 21:36:46 +0900

    $ bin/rails db:migrate

--- spec/factories/ad_media.rb
+++ spec/factories/ad_media.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: ad_media # 広告媒体マスターテーブル
+#
+#  id                 :bigint           not null, primary key
+#  name(媒体名)       :string           not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  company_id(企業ID) :bigint           not null
+#
+# Indexes
+#
+#  index_ad_media_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
 FactoryBot.define do
   factory :ad_medium do
     name { 'MyString' }
