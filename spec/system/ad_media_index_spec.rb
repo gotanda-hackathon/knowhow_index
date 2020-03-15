6c768a63099c54b4b8e733482c35b9b9028729c2
川島慎悟 <kawashingo1990@gmail.com>
2020-03-15 18:06:32 +0900

    CSV機能のRSpecを拡張

--- spec/system/ad_media_index_spec.rb
+++ spec/system/ad_media_index_spec.rb
@@ -26,6 +26,8 @@ describe 'フロント画面：広告媒体一覧', type: :system do
       it_behaves_like 'ボタンエクスペクテーション：not_be_able_to_new'
       it_behaves_like 'ボタンエクスペクテーション：not_be_able_to_edit'
       it_behaves_like 'ボタンエクスペクテーション：not_be_able_to_destroy'
+      it_behaves_like 'ボタンエクスペクテーション：not_be_able_to_csv_import'
+      it_behaves_like 'ボタンエクスペクテーション：not_be_able_to_csv_export'
     end
 
     context '権限が grader のとき' do
@@ -33,6 +35,8 @@ describe 'フロント画面：広告媒体一覧', type: :system do
 
       it_behaves_like '一覧表示'
       it_behaves_like 'ボタンエクスペクテーション：be_able_to_new'
+      it_behaves_like 'ボタンエクスペクテーション：be_able_to_csv_import'
+      it_behaves_like 'ボタンエクスペクテーション：be_able_to_csv_export'
 
       it '編集ボタンがログインアカウントと同じ企業に紐づくAdMediumの数だけあること' do
         expect(page).to have_content('編集', count: AdMedium.same_company_with(login_user).count)
@@ -40,6 +44,20 @@ describe 'フロント画面：広告媒体一覧', type: :system do
 
       it '削除ボタンがログインアカウントと同じ企業に紐づくAdMediumの数だけあること' do
         expect(page).to have_content('削除', count: AdMedium.same_company_with(login_user).count)
+      end
+
+      context 'ファイルをセットせずにCSV取込ボタンを押すとき' do
+        before do
+          click_on 'CSV取込'
+        end
+
+        it 'エラーになること' do
+          aggregate_failures do
+            expect(page).to have_current_path company_ad_media_path(login_user.company)
+            expect(page).to have_css '.red.lighten-4'
+            expect(page).to have_content 'CSVファイルを取り込んでください'
+          end
+        end
       end
     end
   end
