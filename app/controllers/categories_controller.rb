05ef4615f857defb811e184d43d9ba8e9bb1323e
sato <sato@asnica.co.jp>
2020-03-26 08:24:03 +0900

    cateogories_一覧ロジック追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
-  def index; end
+  def index
+    condition = current_user.get_search_condition(code: 'category', params: search_params.to_unsafe_h)
+    @search_form = CategorySearchForm.new(condition)
+    @categories  = @search_form.search(current_user).paginated(params[:page]).decorate
+  end
 
   def new; end
 
@@ -12,4 +16,10 @@ class CategoriesController < ApplicationController
   def update; end
 
   def destroy; end
+
+  private
+
+  def search_params
+    params.fetch(:search_form, {}).permit(:name)
+  end
 end
