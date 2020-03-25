e36cd28b2471284e7b6bc8f7f3d13e9156e27a0f
sato <sato@asnica.co.jp>
2020-03-26 08:40:11 +0900

    categories#編集ロジック実装

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[edit update destroy]
+  before_action :not_accessible_different_company_data, only: %i[edit update destroy]
+
   def index
     condition = current_user.get_search_condition(code: 'category', params: search_params.to_unsafe_h)
     @search_form = CategorySearchForm.new(condition)
@@ -24,11 +27,22 @@ class CategoriesController < ApplicationController
 
   def edit; end
 
-  def update; end
+  def update
+    if @category.update(category_params)
+      redirect_to company_categories_url(current_user.company, @category), flash: { green: t('views.flash.update_success') }
+    else
+      flash.now[:red] = t('views.flash.update_danger')
+      render :edit
+    end
+  end
 
   def destroy; end
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name, :company_id)
@@ -36,5 +50,9 @@ class CategoriesController < ApplicationController
 
   def search_params
     params.fetch(:search_form, {}).permit(:name)
+  end
+
+  def not_accessible_different_company_data
+    redirect_to root_url, flash: { red: t('views.flash.not_accessible_different_company_data') } if current_user.company != @category.company
   end
 end
