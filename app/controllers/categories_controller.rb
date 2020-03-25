8b987b564c6293dd3beb2a8bd5aab4eb8744300c
sato <sato@asnica.co.jp>
2020-03-26 07:58:26 +0900

    $ rails generate controller Categories index new create edit update destroy

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class CategoriesController < ApplicationController
+  def index; end
+
+  def new; end
+
+  def create; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+end
