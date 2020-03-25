65b2acceabb49e2d0ea1837c47fb854373811149
sato <sato@asnica.co.jp>
2020-03-26 07:44:18 +0900

    add categories annotate

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,23 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: categories # カテゴリマスターテーブル
+#
+#  id                 :bigint           not null, primary key
+#  name(カテゴリ名)   :string           not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  company_id(企業ID) :bigint           not null
+#
+# Indexes
+#
+#  index_categories_on_company_id  (company_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
 class Category < ApplicationRecord
   include CompanyMatchable
 
