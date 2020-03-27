68a1b860482d53339a6d667cc083438fe8c3b86d
sato <sato@asnica.co.jp>
2020-03-27 22:51:15 +0900

    クライアントマスタ作成

---
+++ spec/factories/clients.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: clients # クライアントマスターテーブル
+#
+#  id                 :bigint           not null, primary key
+#  name(媒体名)       :string           not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  company_id(企業ID) :bigint           not null
+#
+# Indexes
+#
+#  index_clients_on_company_id           (company_id)
+#  index_clients_on_name_and_company_id  (name,company_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
+FactoryBot.define do
+  factory :client do
+    name { 'MyString' }
+    company { nil }
+  end
+end
