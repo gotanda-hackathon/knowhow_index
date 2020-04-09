1a15cddfc4a0fb760aa1033a9982dc9a887dea2c
斉藤茂之 <saitou@asnica.co.jp>
2020-04-09 23:54:20 +0900

    no message

---
+++ spec/factories/indicator.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: ad_media # 広告媒体マスターテーブル
+#
+#  id                 :bigint           not null, primary key
+#  name(改善指標名)       :string           not null
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  company_id(企業ID) :bigint           not null
+#
+# Indexes
+#
+#  index_indicators_on_company_id           (company_id)
+#  index_indicators_on_name_and_company_id  (name,company_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (company_id => companies.id)
+#
+FactoryBot.define do
+  factory :indicator do
+    sequence(:name) { |n| "改善指標名#{n}" }
+    association :company
+  end
+end
