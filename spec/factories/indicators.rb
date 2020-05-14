d3968ae543e56424c2949d328562bb9f88d3a7cc
斉藤茂之 <saitou@asnica.co.jp>
2020-05-14 10:10:15 +0900

    no message

--- spec/factories/indicators.rb
+++ spec/factories/indicators.rb
@@ -2,18 +2,17 @@
 
 # == Schema Information
 #
-# Table name: indicator # 改善指標マスターテーブル
+# Table name: indicators # 改善指標マスターテーブル
 #
 #  id                 :bigint           not null, primary key
-#  name(改善指標名)       :string           not null
+#  name(改善指標名)   :string           not null
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #  company_id(企業ID) :bigint           not null
 #
 # Indexes
 #
-#  index_indicator_on_company_id           (company_id)
-#  index_indicator_on_name_and_company_id  (name,company_id) UNIQUE
+#  index_indicators_on_company_id  (company_id)
 #
 # Foreign Keys
 #
